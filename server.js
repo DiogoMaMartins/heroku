9d5d01088ef6cfd98198c616fc91099925ee8868
DiogoMaMartins <mamartinsdiogo@gmail.com>
2019-01-12 14:58:47 +0100

    Test

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 function onRequest(request, response) {
     response.writeHead(200, {'Content-Type': 'text/html'});
-    fs.readFile('./render-page/index.html', null, function(error, data){
+    fs.readFile('./public/index.html', null, function(error, data){
         if (error){
             response.writeHead(404);
             response.write('File not found');
